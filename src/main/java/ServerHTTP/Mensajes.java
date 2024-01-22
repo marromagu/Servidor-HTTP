681d829c31bb4b05fb0ec0a83797b760c9c3a915
marromagu <mromagu2611@g.educaand.es>
2024-01-22 14:14:02 +0100

    Post y get

---
+++ src/main/java/ServerHTTP/Mensajes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ServerHTTP;
+
+/**
+ *
+ * @author IMCG
+ */
+//Mensajes que intercambia el Servidor con el Cliente según protocolo HTTP
+public class Mensajes {
+
+    public static final String lineaInicial_OK = "HTTP/1.1 200 OK";
+    public static final String lineaInicial_NotFound
+            = "HTTP/1.1 404 Not Found";
+//  public static final String lineaInicial_BadRequest =
+//          "HTTP/1.1 400 Bad Request";
+
+}
---
+++ src/main/java/ServerHTTP/Paginas.java
@@ -0,0 +1,94 @@
+package ServerHTTP;
+
+/**
+ * ****************************************************************************
+ * clase no instanciable donde se definen algunos valores finales
+ *
+ * @author IMCG
+ */
+public class Paginas {
+
+    public static final String primeraCabecera
+            = "Content-Type:text/html;charset=UTF-8";
+    //contenido index
+    public static final String html_index = "<html>"
+            + "<head><title>index</title></head>"
+            + "<body>"
+            + "<h1>¡Enhorabuena!</h1>"
+            + "<p>Tu servidor HTTP mínimo funciona correctamente</p>"
+            + "<p><a href=\"http://localhost:8066/quijote\">El Quijote</a></p>"
+            + "<p><a href=\"http://localhost:8066/formularioGet\">Formulario Get</a></p>"
+            + "<p><a href=\"http://localhost:8066/formularioPost\">Formulario Post</a></p>"
+            + "<p><a href=\"http://localhost:8066/formularioRespuesta\">Respuesta</a></p>"
+            + "</body>"
+            + "</html>";
+    //contenido quijote
+    public static final String html_quijote = "<html>"
+            + "<head><title>quijote</title></head>"
+            + "<body>"
+            + "<h1>Así comienza el Quijote</h1>"
+            + "<p>En un lugar de la Mancha, de cuyo nombre no quiero "
+            + "acordarme, no ha mucho tiempo que vivía un hidalgo de los "
+            + "de lanza en astillero, adarga antigua, rocín flaco y galgo "
+            + "corredor. Una olla de algo más vaca que carnero, salpicón "
+            + "las más noches, duelos y quebrantos (huevos con tocino) los "
+            + "sábados, lentejas los viernes, algún palomino de añadidura "
+            + "los domingos, consumían las tres partes de su hacienda. El "
+            + "resto della concluían sayo de velarte (traje de paño fino), "
+            + "calzas de velludo (terciopelo) para las fiestas con sus "
+            + "pantuflos de lo mismo, y los días de entresemana se honraba "
+            + "con su vellorí (pardo de paño) de lo más fino. Tenía en su "
+            + "casa una ama que pasaba de los cuarenta, y una sobrina que "
+            + "no llegaba a los veinte, y un mozo de campo y plaza, que "
+            + "así ensillaba el rocín como tomaba la podadera...</p>"
+            + "</body>"
+            + "</html>";
+    //Fomulario por GET
+    public static final String html_GET = "<html>"
+            + "<head>"
+            + "    <title>Formulario Get</title>"
+            + "</head>"
+            + "<body>"
+            + "    <form action=\"http://localhost:8066/formularioRespuesta\" method=\"get\" id=\"miFormulario\">"
+            + "        <label for=\"nombre\">Nombre:</label>"
+            + "        <input type=\"text\" id=\"nombre\" name=\"nombre\" required>"
+            + "        <label for=\"numeroMultiplica\">Número a multiplicar:</label>"
+            + "        <input type=\"number\" id=\"numeroMultiplica\" name=\"numeroMultiplica\" required>"
+            + "        <button type=\"submit\">Enviar</button>"
+            + "    </form>"
+            + "</body>"
+            + "</html>";
+    //Fomulario por GET
+    public static final String html_POST = "<html>"
+            + "<head>"
+            + "    <title>Formulario Post</title>"
+            + "</head>"
+            + "<body>"
+            + "    <form action=\"http://localhost:8066/formularioRespuesta\" method=\"post\" id=\"miFormulario\">"
+            + "        <label for=\"nombre\">Nombre:</label>"
+            + "        <input type=\"text\" id=\"nombre\" name=\"nombre\" required>"
+            + "        <label for=\"numeroMultiplica\">Número a multiplicar:</label>"
+            + "        <input type=\"number\" id=\"numeroMultiplica\" name=\"numeroMultiplica\" required>"
+            + "        <button type=\"submit\">Enviar</button>"
+            + "    </form>"
+            + "</body>"
+            + "</html>";
+    //Fomulario por GET
+    public static final String html_Respuesta = "<html>"
+            + "<head>"
+            + "    <title>Formulario Post</title>"
+            + "</head>"
+            + "<body>"
+            +"<h1>RESPUESTA</h1>"
+            + "</body>"
+            + "</html>";
+    //contenido noEncontrado
+    public static final String html_noEncontrado = "<html>"
+            + "<head><title>noEncontrado</title></head>"
+            + "<body>"
+            + "<h1>¡ERROR! Página no encontrada</h1>"
+            + "<p>La página que solicitaste no existe en nuestro "
+            + "servidor</p>"
+            + "</body>"
+            + "</html>";
+}
